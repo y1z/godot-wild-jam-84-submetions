dd70114dbc1edbedf9552c069eea96f22fdf20dd
y1z <whyis2cool@gmail.com>
2025-08-09 23:35:04 -0600

    Critter can detect the mouse

--- Scripts/critter.gd
+++ Scripts/critter.gd
@@ -10,12 +10,16 @@ enum critter_type
 # region const
 const minimum_speed = 400.0;
 
+@export_group("VARIABLES")
 @export var type : critter_type = critter_type.NONE
 @export var direction :Vector2;
-@export var target =position 
+@export var target :Vector2 = position 
 @export var speed :float = 0 
 
+var collision_shape : RectangleShape2D;
+
 func _ready() -> void:
+	collision_shape = %"Critter Hitbox".shape;
 	if speed < minimum_speed :
 		speed = minimum_speed
 	
@@ -30,6 +34,11 @@ func _input(event):
 func _physics_process(delta):
 	velocity = position.direction_to(target) * speed
 	# look_at(target)
+	#collision_shape.get_rect().get
+	
+	if collision_shape.get_rect().has_point(to_local(get_global_mouse_position()) ):
+		print("is inside rectangle")
+		pass
 	
 	if position.distance_to(target) > 10:
 		move_and_slide()
