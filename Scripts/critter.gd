ae98c2203a91c437c94995b9dd53f401f1dab8e4
y1z <whyis2cool@gmail.com>
2025-08-09 23:14:14 -0600

    things

--- Scripts/critter.gd
+++ Scripts/critter.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CharacterBody2D
 class_name Critter
 
 enum critter_type
@@ -7,5 +7,29 @@ enum critter_type
 	Safe,
 	Danger
 }
+# region const
+const minimum_speed = 400.0;
 
-var type : critter_type = critter_type.NONE
+@export var type : critter_type = critter_type.NONE
+@export var direction :Vector2;
+@export var target =position 
+@export var speed :float = 0 
+
+func _ready() -> void:
+	if speed < minimum_speed :
+		speed = minimum_speed
+	
+	
+	pass
+
+func _input(event):
+	# Use is_action_pressed to only accept single taps as input instead of mouse drags.
+	if event.is_action_pressed(&"l_click"):
+		target = get_global_mouse_position()
+
+func _physics_process(delta):
+	velocity = position.direction_to(target) * speed
+	# look_at(target)
+	
+	if position.distance_to(target) > 10:
+		move_and_slide()
