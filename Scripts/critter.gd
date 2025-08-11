e7f5925c9e3024a8bba3ff722e86107081205a4c
y1z <whyis2cool@gmail.com>
2025-08-10 21:37:40 -0600

    Update export

--- Scripts/critter.gd
+++ Scripts/critter.gd
@@ -7,15 +7,17 @@ enum critter_type
 	Safe,
 	Danger
 }
-# region const
+#region const
 const minimum_speed = 400.0;
-# endregion 
+#endregion 
 
+#region var
 @export_group("VARIABLES")
 @export var type : critter_type = critter_type.NONE
 @export var direction :Vector2;
 @export var target :Vector2 = position 
 @export var speed :float = 0 
+#endregion 
 
 var collision_shape : RectangleShape2D;
 
@@ -28,14 +30,13 @@ func _ready() -> void:
 	pass
 
 func _input(event):
-	# Use is_action_pressed to only accept single taps as input instead of mouse drags.
 	if event.is_action_pressed(&"l_click"):
 		target = get_global_mouse_position()
+		
+		
 
 func _physics_process(delta):
 	velocity = position.direction_to(target) * speed
-	# look_at(target)
-	#collision_shape.get_rect().get
 	
 	if collision_shape.get_rect().has_point(to_local(get_global_mouse_position()) ):
 		print("is inside rectangle")
