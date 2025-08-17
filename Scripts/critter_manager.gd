2ef20c03d50dddda56625512639104d38ac26c17
y1z <whyis2cool@gmail.com>
2025-08-17 14:01:59 -0600

    Manager for the critters

---
+++ Scripts/critter_manager.gd
@@ -0,0 +1,36 @@
+extends Node2D
+var critter_template :PackedScene = preload("uid://bwksi02isilta")
+var critter_danger :PackedScene = preload("uid://dh5k032kukcr5" )
+var critter_count :int= 0;
+
+func _ready() -> void:
+	var error_code = EventBus.critter_count_down.connect(on_criter_count_down)
+	if error_code != OK:
+		printerr("error code [%s]" % error_code)
+	critter_count = 0;
+	for i in range(1,10):
+		var critter_temp:Node;
+		if i % 2 == 0:
+			critter_temp = critter_template.instantiate()
+		else:
+			critter_temp = critter_danger.instantiate()
+			
+		add_child(critter_temp)
+		critter_count += 1;
+		pass
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if critter_count < 1:
+		get_tree().reload_current_scene()
+		pass
+	
+	pass
+
+func on_criter_count_down(should_count_down:bool)->void:
+	if should_count_down != true :
+		return;
+	critter_count -= 1;
+	pass
